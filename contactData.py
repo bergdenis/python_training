eeba308c4d44ae0948d7dbf4059903585970f68d
Denis Berg <bergdenis@seznam.cz>
2023-06-07 18:35:20 +0200

    Задание №3 (тестовый вариант)

--- contactData.py
+++ contactData.py
@@ -1,7 +1,7 @@
-class Properties:
+class ContactData:
 
-    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work,
-                       fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2, phone2,
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_number, mobile_number, work_number,
+                       fax_number, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2, phone2,
                        notes):
         self.firstname = firstname
         self.middlename = middlename
@@ -10,10 +10,10 @@ class Properties:
         self.title = title
         self.company = company
         self.address = address
-        self.home = home
-        self.mobile = mobile
-        self.work = work
-        self.fax = fax
+        self.home_number = home_number
+        self.mobile_number = mobile_number
+        self.work_number = work_number
+        self.fax_number = fax_number
         self.email = email
         self.email2 = email2
         self.email3 = email3
