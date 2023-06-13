aa0e8e5dd3665dfe2c0a58832396bc3f3371cd7b
Denis Berg <bergdenis@seznam.cz>
2023-06-13 21:44:05 +0200

    Задание №4

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.wd.maximize_window()
 
     def open_home_page(self):
         wd = self.wd
