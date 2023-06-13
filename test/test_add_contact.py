21c22ebec2edee554d028b0e2d8da6b284c7a5cc
Denis Berg <bergdenis@seznam.cz>
2023-06-13 21:52:32 +0200

    Созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contactData import ContactData
-from application import Application
+from model.contactData import ContactData
+from fixture.application import Application
 
 
 @pytest.fixture
