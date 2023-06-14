cf6bb6502257ca028126bf4b92d8cfa09297c47a
Denis Berg <bergdenis@seznam.cz>
2023-06-14 13:14:23 +0200

    Задание №7

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,5 +1,8 @@
+import time
+
 
 def test_delete_first_contact(app):
+    time.sleep(1)
     app.session.login(username="admin", password="secret")
     app.contact.delete_first_contact()
     app.session.logout()
