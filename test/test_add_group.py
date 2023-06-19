40d96e360030267289ddb39384009e49f6fa2ea0
Denis Berg <bergdenis@seznam.cz>
2023-06-19 23:47:14 +0200

    Задание №8

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,5 +5,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="test", header="teest", footer="teeest"))
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
