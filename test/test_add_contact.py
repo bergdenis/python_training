dbcdd5026fdd6e891222e1e3f016d622f19a2036
Denis Berg <bergdenis@seznam.cz>
2023-06-19 23:08:39 +0200

    Реализована валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,6 @@ from model.contactData import ContactData
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(ContactData(firstname="fn", middlename="mn", lastname="ln", nickname="nn", title="ttl", company="cmp", address="adr1", home_number="111111", mobile_number="222222", work_number="333333", fax_number="444444",
                                    email="mail@mail.com", email2="mail2@mail.com", email3="mail3@mail.com", homepage="www.hopg.com", bday="7", bmonth="July", byear="1977",
                                    aday="10", amonth="October", ayear="2010", address2="adr2", phone2="555555", notes="test"))
-    app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,8 +2,6 @@ from model.contactData import ContactData
 
 
 def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(ContactData(firstname="first name", middlename="middle name", lastname="last name", nickname="nick name", title="title", company="company", address="address", home_number="123456", mobile_number="234567", work_number="345678", fax_number="456789",
                                    email="mail@mail.cz", email2="mail2@mail.cz", email3="mail3@mail.cz", homepage="www.homepage.com", bday="8", bmonth="August", byear="1988",
                                    aday="11", amonth="November", ayear="2011", address2="address2", phone2="567890", notes="text text"))
-    app.session.logout()
