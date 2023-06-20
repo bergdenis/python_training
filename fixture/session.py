537bb49a15dd12e6b402c71017412106e80f3bd6
Denis Berg <bergdenis@seznam.cz>
2023-06-21 00:04:01 +0200

    Задание №8 (Попытка 2)

--- fixture/session.py
+++ fixture/session.py
@@ -27,7 +27,7 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element(By.XPATH, "//div[@id='top']/form/b").text == ("+username+")
+        return wd.find_element(By.XPATH, "//div[@id='top']/form/b").text == "("+username+")"
 
     def ensure_logout(self):
         wd = self.app.wd
