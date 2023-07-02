d78b363945134db796275183ca80e274f7cedb1c
Denis Berg <bergdenis@seznam.cz>
2023-07-02 23:32:27 +0200

    Тест доработан для случайного выбора контакта

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -1,14 +1,22 @@
-import re
+# import re
+from random import randrange
+from model.contactData import ContactData
 
 
 def test_contact_info_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    if app.contact.count() == 0:
+        app.contact.create(ContactData(firstname="Test"))
+    old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
 
 
 def clear(s):
