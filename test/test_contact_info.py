04067a369263f420a26f315ded59480bc2006e4d
Denis Berg <bergdenis@seznam.cz>
2023-07-02 22:34:21 +0200

    Создан тест в котором реализованы проверки для всех полей контакта на главной странице

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -1,33 +1,13 @@
 import re
 
 
-def test_last_name_on_home_page(app):
+def test_contact_info_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-
-
-def test_first_name_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-
-
-def test_address_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.address == contact_from_edit_page.address
-
-
-def test_emails_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-
-
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
@@ -35,14 +15,14 @@ def clear(s):
     return re.sub("[() -]", "", s)
 
 
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            filter(lambda x: x is not None,
+                                   [contact.email, contact.email2, contact.email3])))
+
+
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.home_number, contact.mobile_number, contact.work_number, contact.secondary_number]))))
-
-
-def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3])))
