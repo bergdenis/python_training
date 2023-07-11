0a492d18963189b980225650acdf9d4d7260ea04
Denis Berg <bergdenis@seznam.cz>
2023-07-12 00:43:58 +0200

    Отделены тестовые данные от сценариев

--- data/add_group.py
+++ data/add_group.py
@@ -5,8 +5,9 @@ import string
 
 constant = [
     Group(name="name1", header="header1", footer="footer1"),
-    Group(name="name2", header="header2", footer="footer2)
+    Group(name="name2", header="header2", footer="footer2")
 ]
+
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
