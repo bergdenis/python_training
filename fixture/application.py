5a2053041ba723624ef97be9a3e6d0755379d88b
Denis Berg <bergdenis@seznam.cz>
2023-07-12 00:17:03 +0200

    Создан конфигурационный файл для описания тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser, base_url, password):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -20,7 +20,6 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
-        self.password = password
 
     def is_valid(self):
         try:
