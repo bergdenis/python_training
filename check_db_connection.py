85a173ff1df4ad8ecf301fa432eb7d46671c0d6a
Denis Berg <bergdenis@seznam.cz>
2023-07-19 21:46:44 +0200

    ORM – Object Relational Mapping

--- check_db_connection.py
+++ check_db_connection.py
@@ -10,14 +10,26 @@
 # finally:
 #     connection.close()
 
-from fixture.db import DbFixture
+# from fixture.db import DbFixture
+#
+# db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+#
+# try:
+#     contacts = db.get_contact_list()
+#     for contact in contacts:
+#         print(contact)
+#     print(len(contacts))
+# finally:
+#     db.destroy()
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+from fixture.orm import ORMFixture
+
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass # db.destroy()
