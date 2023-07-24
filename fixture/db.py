9006bc6b02f5725618478c53e2e5093456402055
Denis Berg <bergdenis@seznam.cz>
2023-07-24 14:00:58 +0200

    Задание №21 (Попытка 2)

--- fixture/db.py
+++ fixture/db.py
@@ -29,7 +29,7 @@ class DbFixture:
         cursor = self.connection.cursor()
         try:
             cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2 "
-                           "from addressbook")
+                           "from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, lastname, address, home, mobile, work, email, email2, email3, phone2) = row
                 list.append(ContactData(id=str(id), firstname=firstname, lastname=lastname, address=address,
