53bf6d7a5d31a04f81b9327f109df24d2598b65e
Denis Berg <bergdenis@seznam.cz>
2023-07-24 16:28:01 +0200

    Реализованы отношения между объектами

--- check_db_connection.py
+++ check_db_connection.py
@@ -10,18 +10,18 @@
 # finally:
 #     connection.close()
 
-from fixture.db import DbFixture
-
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
-
-try:
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
-finally:
-    db.destroy()
-
+# from fixture.db import DbFixture
+#
+# db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+#
+# try:
+#     contacts = db.get_contact_list()
+#     for contact in contacts:
+#         print(contact)
+#     print(len(contacts))
+# finally:
+#     db.destroy()
+#
 # from fixture.orm import ORMFixture
 #
 # db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
@@ -33,3 +33,16 @@ finally:
 #     print(len(l))
 # finally:
 #     pass # db.destroy()
+
+from fixture.orm import ORMFixture
+from model.group import Group
+
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+try:
+    l = db.get_contacts_not_in_group(Group(id="319"))
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
+    pass
