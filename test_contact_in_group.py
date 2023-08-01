4c141f8459bb5d465926b43fcffc80c40f65573e
Denis Berg <bergdenis@seznam.cz>
2023-08-01 23:23:26 +0200

    Задание №22 (Попытка 2)

--- test_contact_in_group.py
+++ test_contact_in_group.py
@@ -5,10 +5,10 @@ import random
 
 def test_add_contact_to_group(app, orm):
     if len(orm.get_group_list()) == 0:
-        app.group.create(Group(name="Test"))
+        app.group.create(Group(name="Temp_group"))
     random_group = random.choice(orm.get_group_list())
     if len(orm.get_contact_list()) == 0 or len(orm.get_contacts_not_in_group(random_group)) == 0:
-        app.contact.create_new(ContactData(firstname="Test"))
+        app.contact.create_new(ContactData(firstname="Temp_group"))
     random_contact = random.choice(orm.get_contacts_not_in_group(random_group))
     app.contact.add_contact_to_group(random_contact, random_group)
     assert random_contact in orm.get_contacts_in_group(random_group)
@@ -16,15 +16,15 @@ def test_add_contact_to_group(app, orm):
 
 def test_delete_contact_from_group(app, orm):
     if len(orm.get_group_list()) == 0:
-        app.group.create(Group(name="Test"))
+        app.group.create(Group(name="Temp_group"))
     random_group = random.choice(orm.get_group_list())
-    if len(orm.get_contact_list()) == 0 or len(orm.get_contacts_not_in_group(random_group)) == 0:
-        app.contact.create_new(ContactData(firstname="Test"))
+    if len(orm.get_contact_list()) == 0:
+        app.contact.create_new(ContactData(firstname="Temp_group"))
     if len(orm.get_contacts_in_group(random_group)) == 0:
         random_contact = random.choice(orm.get_contacts_not_in_group(random_group))
         app.contact.add_contact_to_group(random_contact, random_group)
         assert random_contact in orm.get_contacts_in_group(random_group)
-        random_contact_for_del = random.choice(orm.get_contacts_in_group(random_group))
-        app.contact.delete_contact_from_group(random_contact_for_del, random_group)
-        assert random_contact_for_del not in orm.get_contacts_in_group(random_group)
-        assert random_contact_for_del in orm.get_contacts_not_in_group(random_group)
+    random_contact_for_del = random.choice(orm.get_contacts_in_group(random_group))
+    app.contact.delete_contact_from_group(random_contact_for_del, random_group)
+    assert random_contact_for_del not in orm.get_contacts_in_group(random_group)
+    assert random_contact_for_del in orm.get_contacts_not_in_group(random_group)
