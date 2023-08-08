bb812d1f8527cef41081fbe39d141e10a07a4b56
Denis Berg <bergdenis@seznam.cz>
2023-08-08 14:18:41 +0200

    Реализованы тесты для добавления, удаления и модификации групп в адресной книге с веб-интерфейсом в стиле BDD

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,9 +1,9 @@
-
 from pytest_bdd import given, when, then, parsers
 from model.group import Group
+import random
 
 
-@given("a group list")
+@given("a group list", target_fixture="group_list")
 def group_list(db):
     return db.get_group_list()
 
@@ -24,3 +24,48 @@ def verify_group_added(db, group_list, new_group):
     new_groups = db.get_group_list()
     old_groups.append(new_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+@given("a non-empty group list", target_fixture="non_empty_group_list")
+def non_empty_group_list(db, app):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="some name"))
+    return db.get_group_list()
+
+
+@given("a random group from the list", target_fixture="random_group")
+def random_group(non_empty_group_list):
+    return random.choice(non_empty_group_list)
+
+
+@when('I delete the group from the list')
+def delete_group(app, random_group):
+    app.group.delete_group_by_id(random_group.id)
+
+
+@then('the new group list is equal to the old group list without the deleted group')
+def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui):
+    old_groups = non_empty_group_list
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(random_group)
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+
+@when("I modify the group from the list")
+def modify_group(app, random_group, new_group):
+    app.group.modify_group_by_id(random_group.id, new_group)
+
+
+@then("the new group list is equal to the old group list with the modified group")
+def verify_group_modified(db, non_empty_group_list, new_group, random_group, app, check_ui):
+    old_groups = non_empty_group_list
+    new_group.id = random_group.id
+    new_groups = db.get_group_list()
+    old_groups.remove(random_group)
+    old_groups.append(new_group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
--- bdd/groups.feature
+++ bdd/groups.feature
@@ -9,3 +9,20 @@ Scenario Outline: Add new group
   | name1 | header1 | footer1 |
   | name2 | header2 | footer2 |
 
+Scenario Outline: Delete a group
+  Given a non-empty group list
+  Given a random group from the list
+  When I delete the group from the list
+  Then the new group list is equal to the old group list without the deleted group
+
+Scenario Outline: Modify a group
+  Given a non-empty group list
+  Given a random group from the list
+  Given a group with <name>, <header> and <footer>
+  When I modify the group from the list
+  Then the new group list is equal to the old group list with the modified group
+
+  Examples:
+  | name  | header  | footer  |
+  | name3 | header3 | footer3 |
+  | name4 | header4 | footer4 |
